c1d98c1fe3adf616fb66fe71e8013ed0c9914ec0
meesha94 <95078931+meesha94@users.noreply.github.com>
2022-10-22 23:05:29 +0100

    added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-import './App.scss';
+import "./App.scss";
+import Home from "./containers/Home/Home";
+import NavBar from "./containers/NavBar/NavBar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Course from "./Pages/Course/Course";
+import EditGreeting from "./containers/EditGreeting/EditGreeting";
+import Course from "./Pages/Course/Course";
+import User from "./Pages/User/User";
 
 const App = () => {
   return (
-    <div className="App">
-       <NavBar className="home__navbar"></NavBar>
-    </div>
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/greetings" element={<Course />} />
+        <Route path="/greeting/user" element={<User />} />
+        <Route path="/greeting/edit/:id" element={<EditGreeting />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
