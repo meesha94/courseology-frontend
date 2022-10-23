6fda028e2ebc582816254d4a6793dcf0b1df4319
meesha94 <95078931+meesha94@users.noreply.github.com>
2022-10-23 02:05:48 +0100

    added logo to navbar

--- src/containers/NavBar/NavBar.jsx
+++ src/containers/NavBar/NavBar.jsx
@@ -1,13 +1,15 @@
 import "./NavBar.scss";
 import { Link } from "react-router-dom";
+import logo from "../../assets/images/logo.jpeg";
 
 const NavBar = () => {
   return(
     <div className="nav">
-    <Link className="nav__item" to={"/"}>
+    <img src={logo} alt="hogwarts school logo" className="nav__item-logo"/>
+    <Link className="nav__item-home" to={"/"}>
       <h2>Home</h2>
     </Link>
-    <Link className="nav__item" to={"/user"}>
+    <Link className="nav__item-user" to={"/user"}>
       <h2>User</h2>
     </Link>
   </div>
