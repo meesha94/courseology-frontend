3f9170f1b96ee004e666a720a663a4c52a443105
meesha94 <95078931+meesha94@users.noreply.github.com>
2022-10-24 12:34:57 +0100

    styling for header and navbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,16 @@
 import hogwarts from "../../assets/images/hogwarts.jpg";
+import logo from "../../assets/images/logo.png";
+import name from "../../assets/images/name.png";
 import "./Header.scss";
 
 const Header = () => {
   return (
     <div className="header__container">
-        <img src={hogwarts} alt=" hogwarts school grounds"/>
-        <h1 className="header__title">Hogwarts School Of Witchcraft and Wizardry</h1>
+        <img src={hogwarts} alt=" hogwarts school grounds" className="header__image"/>
+        <div className="header__heading">
+        <img src={logo} alt="hogwarts school logo" className="header__logo"/>
+        <img src={name} alt="school name" className="header__title"/>
+        </div>
     </div>
   )
 }
--- src/containers/NavBar/NavBar.jsx
+++ src/containers/NavBar/NavBar.jsx
@@ -1,11 +1,10 @@
 import "./NavBar.scss";
 import { Link } from "react-router-dom";
-import logo from "../../assets/images/logo.png";
 
 const NavBar = () => {
   return(
     <div className="nav">
-    <img src={logo} alt="hogwarts school logo" className="nav__item-logo"/>
+    
     <Link className="nav__item-home" to={"/"}>
       <h2>Home</h2>
     </Link>
